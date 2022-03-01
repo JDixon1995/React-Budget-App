d7568f57b963f76ad90074d3c440d93d180a2909
John D <jddhalogamer@gmail.com>
2022-03-01 17:59:49 -0600

    added functionality to form modal.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { Button, Stack } from 'react-bootstrap'
 import Container from 'react-bootstrap/Container'
 import { BudgetCard } from './components/BudgetCard';
+import AddBudgetModal from './components/AddBudgetModal';
+import { useState } from 'react'
 
 function App() {
+
+  const [showAddBudgetModal, setShowAddBudgetModal ] = useState(false)
+
   return (
+    <>
     <Container className='my-4'>
       <Stack direction='horizontal' gap='2' className='mb-4'>
         <h1 className='me-auto'>Budgets</h1>
-        <Button variant='primary'>Add Budget</Button>
+        <Button variant='primary' onClick={() => setShowAddBudgetModal(true) }>Add Budget</Button>
         <Button variant='outline-primary'>Add Expense</Button>
       </Stack>
       <div style={{ 
@@ -23,6 +29,9 @@ function App() {
         ></BudgetCard>
       </div>
     </Container>
+    <AddBudgetModal show={showAddBudgetModal} handleClose={() => 
+    setShowAddBudgetModal(false)} />
+    </>
   );
 }
 
